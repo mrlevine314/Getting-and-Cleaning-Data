2279da26a9c9eae86e240582171ccfce796ec1e3
mrlevine314 <matthewlevine14@gmail.com>
2016-02-28 15:48:28 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,22 @@
-#run_analysis
+# Load packages for use
+
+install.packages ("reshape2")
+library(reshape2)
+
+# Save filename for efficiency and correctness
+file <- "dataset.zip"
+
+# Check if file already exists. If not, downloads file. 
+if (!file.exists(file)) {
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileUrl,file, method = "curl")
+}
+
+#
+  
+  
+  
+  
+  
+  els <- read.table("UCI HAR Dataset/activity_labels.txt")
+activity_labels[,2] <- 
